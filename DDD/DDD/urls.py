3d016bdd468daf6974c7f580262daccfd5c17ba3
Bri Y <bri@bris-air.attlocal.net>
2020-05-04 10:46:44 -0700

    set up urls.py and views

--- DDD/DDD/urls.py
+++ DDD/DDD/urls.py
@@ -1,4 +1,4 @@
-"""dungeons_daddies_and_divas URL Configuration
+"""DDD URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -14,9 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main_app.urls')),
 ]
