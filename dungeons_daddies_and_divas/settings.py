966907b9962b76f2382537a02922797b1468ee27
Danny <danny@admins-mbp.attlocal.net>
2020-05-06 10:58:59 -0700

    Added styling to home and forms

--- dungeons_daddies_and_divas/settings.py
+++ dungeons_daddies_and_divas/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
+
 LOGIN_REDIRECT_URL = '/posts/'
 
 LOGOUT_REDIRECT_URL = '/'
